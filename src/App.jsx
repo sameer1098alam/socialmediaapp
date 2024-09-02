2f06ab2a59d95b9eee8014cfb7aff1896bb4f17a
Sameer Alam <104647142+sameer1098alam@users.noreply.github.com>
2024-09-02 19:54:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button from "@mui/material/Button";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      Welcome
+      <Button variant="contained">Hello world</Button>;
+    </>
+  );
+}
+
+export default App;
